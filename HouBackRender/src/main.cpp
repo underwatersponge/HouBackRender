e43dd052fcc66e8b579c9651326ebe3a8b5b2bd8
underwatersponge <2593214969@qq.com>
2026-08-22 18:59:05 +0800

    BaseWindow

---
+++ HouBackRender/src/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+
+static void GlfwErrorCallback(int error, const char* description)
+{
+	std::cout << "GLFW ERROR:" << error << description<< "\n";
+}
+
+int main()
+{
+	glfwSetErrorCallback(GlfwErrorCallback);
+	
+	if (!glfwInit())
+		return 1;
+	const char* glsl_version = nullptr;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	// glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
+	GLFWwindow* window = glfwCreateWindow((int)(1280*main_scale), (int)(800*main_scale), "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "can not load opengl function " << std::endl;
+		return -1;
+	}
+	
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+	
+	ImGui::StyleColorsDark();
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.ScaleAllSizes(main_scale);
+	style.FontScaleDpi = main_scale;
+	
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+	
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
+		{
+			ImGui_ImplGlfw_Sleep(10);
+			continue;
+		}
+		
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+			ImGui::End();
+		}
+		
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
+		}
+		
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
---
+++ premake5.lua
@@ -0,0 +1,57 @@
+workspace "HouBackRender"
+    architecture "x64"
+    configurations {"Debug","Release"}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+Third = {}
+Third["DearImgui"] = "%{prj.name}/Third/DearImgui"
+Third["GLFW"] = "%{prj.name}/Third/GLFW"
+Third["glad"] = "%{prj.name}/Third/glad"
+
+project "HouBackRender"
+    location "HouBackRender"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin/" .. outputdir .. "/%{prj.name}")
+    files{
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+
+        "%{Third.glad}/src/glad.c",
+        "%{Third.DearImgui}/*.h",
+        "%{Third.DearImgui}/*.cpp",
+        "%{Third.DearImgui}/backends/imgui_impl_glfw.h",
+        "%{Third.DearImgui}/backends/imgui_impl_glfw.cpp",
+        "%{Third.DearImgui}/backends/imgui_impl_opengl3.h",
+        "%{Third.DearImgui}/backends/imgui_impl_opengl3.cpp",
+
+    }
+
+    includedirs
+    {
+        "%{Third.glad}/include",
+        "%{Third.GLFW}/include",
+        "%{Third.DearImgui}",
+        "%{Third.DearImgui}/backends",
+    }
+
+    libdirs
+    {
+        "%{Third.GLFW}",
+    }
+    
+    links
+    {
+        "opengl32.lib",
+        "glfw3.lib",
+    }
+    filter "system:windows"
+        cppdialect "C++17"
+        systemversion "latest"
+    filter "configurations:Debug"
+        symbols "On"
+    filter "configurations:Release"
+        optimize "On"
